63e20d8fb3b01fd58d48af55f383a0565d90bbe2
Siraaj Khandkar <siraaj@khandkar.net>
2021-12-02 18:35:08 -0500

    Convert to result:t/2 and add more test cases

--- src/blockchain_term.erl
+++ src/blockchain_term.erl
@@ -15,66 +15,106 @@
 from_bin(<<_/binary>>) ->
     error(not_implemented).
 
--spec binary_to_proplist(binary()) -> term().
+-spec binary_to_proplist(binary()) ->
+    {ok, t()} | {error, Reason} when Reason :: any(). % TODO be more specific
 binary_to_proplist(<<131, 108, Length:32/integer-unsigned-big, Rest/binary>>) ->
-    {Res, <<>>} = decode_list(Rest, Length, []),
-    Res.
+    case decode_list(Rest, Length, []) of
+        {ok, {T, <<>>}} ->
+            {ok, T};
+        {error, _}=Err ->
+            Err
+    end.
 
+-spec decode_list(binary(), integer(), list()) ->
+    {ok, {[term()], binary()}} | {error, any()}.
 decode_list(<<106, Rest/binary>>, 0, Acc) ->
-    {lists:reverse(Acc), Rest};
+    {ok, {lists:reverse(Acc), Rest}};
 decode_list(Rest, 0, Acc) ->
     %% tuples don't end with an empty list
-    {lists:reverse(Acc), Rest};
+    {ok, {lists:reverse(Acc), Rest}};
 decode_list(<<104, Size:8/integer, Bin/binary>>, Length, Acc) ->
-    {List, Rest} = decode_list(Bin, Size, []),
-    decode_list(Rest, Length - 1, [list_to_tuple(List)|Acc]);
+    case decode_list(Bin, Size, []) of
+        {ok, {List, Rest}} ->
+            decode_list(Rest, Length - 1, [list_to_tuple(List)|Acc]);
+        {error, _}=Err ->
+            Err
+    end;
 decode_list(<<108, L2:32/integer-unsigned-big, Bin/binary>>, Length, Acc) ->
-    {List, Rest} = decode_list(Bin, L2, []),
-    decode_list(Rest, Length - 1, [List|Acc]);
+    case decode_list(Bin, L2, []) of
+        {ok, {List, Rest}} ->
+            decode_list(Rest, Length - 1, [List|Acc]);
+        {error, _}=Err ->
+            Err
+    end;
 decode_list(<<106, Rest/binary>>, Length, Acc) ->
     %% sometimes there's an embedded empty list
     decode_list(Rest, Length - 1, [[] |Acc]);
 decode_list(Bin, Length, Acc) ->
-    {Val, Rest} = decode_value(Bin),
-    decode_list(Rest, Length -1, [Val|Acc]).
+    case decode_value(Bin) of
+        {ok, {Val, Rest}} ->
+            decode_list(Rest, Length -1, [Val|Acc]);
+        {error, _}=Err ->
+            Err
+    end.
 
+-spec decode_value(binary()) ->
+    {ok, {term(), binary()}} | {error, any()}.
 decode_value(<<97, Integer:8/integer, Rest/binary>>) ->
-    {Integer, Rest};
+    {ok, {Integer, Rest}};
 decode_value(<<98, Integer:32/integer-big, Rest/binary>>) ->
-    {Integer, Rest};
+    {ok, {Integer, Rest}};
 decode_value(<<100, AtomLen:16/integer-unsigned-big, Atom:AtomLen/binary, Rest/binary>>) ->
-    {binary_to_atom(Atom, latin1), Rest};
+    {ok, {binary_to_atom(Atom, latin1), Rest}};
 decode_value(<<109, Length:32/integer-unsigned-big, Bin:Length/binary, Rest/binary>>) ->
-    {Bin, Rest};
+    {ok, {Bin, Rest}};
 decode_value(<<110, N:8/integer, Sign:8/integer, Int:N/binary, Rest/binary>>) ->
+    %% TODO Why bother calling decode_bigint before checking Sign?
     case decode_bigint(Int, 0, 0) of
         X when Sign == 0 ->
-            {X, Rest};
+            {ok, {X, Rest}};
         X when Sign == 1 ->
-            {X * -1, Rest}
+            {ok, {X * -1, Rest}};
+        X ->
+            {error, {invalid_sign_for_bigint, Sign, X}}
     end;
 decode_value(<<111, N:32/integer-unsigned-big, Sign:8/integer, Int:N/binary, Rest/binary>>) ->
+    %% TODO Why bother calling decode_bigint before checking Sign?
     case decode_bigint(Int, 0, 0) of
         X when Sign == 0 ->
-            {X, Rest};
+            {ok, {X, Rest}};
         X when Sign == 1 ->
-            {X * -1, Rest}
+            {ok, {X * -1, Rest}}
     end;
 decode_value(<<116, Arity:32/integer-unsigned-big, MapAndRest/binary>>) ->
-    decode_map(MapAndRest, Arity, #{}).
+    decode_map(MapAndRest, Arity, #{});
+%% TODO 107 - what is it? string?
+decode_value(<<Code/integer, _Rest/binary>>) ->
+    {error, {unknown_code, Code}}.
 
+-spec decode_bigint(binary(), integer(), integer()) ->
+    integer().
+    %{ok, integer()} | {error, any()}.
 decode_bigint(<<>>, _, Acc) ->
     Acc;
 decode_bigint(<<B:8/integer, Rest/binary>>, Pos, Acc) ->
     decode_bigint(Rest, Pos + 1, Acc + (B bsl (8 * Pos))).
 
+-spec decode_map(binary(), integer(), map()) ->
+    {ok, {map(), binary()}} | {error, any()}.
 decode_map(Rest, 0, Acc) ->
-    {Acc, Rest};
+    {ok, {Acc, Rest}};
 decode_map(Bin, Arity, Acc) ->
-    {Key, T1} = decode_value(Bin),
-    {Value, T2} = decode_value(T1),
-    decode_map(T2, Arity - 1, maps:put(Key, Value, Acc)).
-
+    case decode_value(Bin) of
+        {ok, {Key, T1}} ->
+            case decode_value(T1) of
+                {ok, {Value, T2}} ->
+                    decode_map(T2, Arity - 1, maps:put(Key, Value, Acc));
+                {error, _}=Err ->
+                    Err
+            end;
+        {error, _}=Err ->
+            Err
+    end.
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -83,10 +123,8 @@ nest(_, X, 0) -> X;
 nest(F, X, N) -> nest(F, F(X), N - 1).
 
 binary_to_proplist_test_() ->
-    %% TODO After converting to exceptionless result:t/2, convert to cartesian product of keys and values.
-    %%      So we can easier explore the input domain.
     [
-        ?_assertMatch(Term, binary_to_proplist(term_to_binary(Term)))
+        ?_assertEqual({ok, Term}, binary_to_proplist(term_to_binary(Term)))
     ||
         Term <- [
             [{1, [<<>>]}],
@@ -100,7 +138,24 @@ binary_to_proplist_test_() ->
             [[]],
             [[[]]],
             [[[[]]]],
-            nest(fun (X) -> [X] end, [], 100)
+            nest(fun (X) -> [X] end, [], 100),
+            [{list, #{}}],
+            [{list, #{1 => 2}}],
+            [{foo, #{1 => 2}}],
+            [{foo, #{bar => baz}}],
+            [{foo, #{<<"bar">> => <<"baz">>}}]
+        ]
+    ]
+    ++
+    [
+        {
+            %% Adding a title because _assertMatch doesn't report values
+            lists:flatten(io_lib:format("Term: ~p", [Term])),
+            ?_assertMatch({error, _}, binary_to_proplist(term_to_binary(Term)))
+        }
+    ||
+        Term <- [
+            [{foo, #{"bar" => "baz"}}]
         ]
     ].
 
